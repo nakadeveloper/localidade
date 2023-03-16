a5c47a82a78126053ba740718994015ac2e7b11f
Fernando Nakagawa <fernandonkgw@gmail.com>
2023-03-16 13:42:22 -0300

    build: configura estrutura de pastas maven

--- src/main/java/fnaka/localidade/LocalidadeApplication.java
+++ src/main/java/fnaka/localidade/LocalidadeApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LocalidadeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LocalidadeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LocalidadeApplication.class, args);
+    }
 
 }
